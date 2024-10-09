7a97dcf2876e95a58b9f1902efa5f824618a1cb0
hakimov0545 <abdulazizhakimov13@gmail.com>
2024-10-09 23:29:02 +0500

    change

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,56 +1,56 @@
 export interface IFileType {
-  path: string;
-  contentType: string;
-  id: string;
+	url: string;
+	contentType: string;
+	id: string;
 }
 
 export interface ICategory {
-  id: string;
-  name: string;
-  description: string;
-  fileResponse: IFileType;
+	id: string;
+	name: string;
+	description: string;
+	fileResponse: IFileType;
 }
 
 export interface IProduct {
-  id: string;
-  name: string;
-  files: IFileType[];
-  sections: ISection[];
+	id: string;
+	name: string;
+	files: IFileType[];
+	sections: ISection[];
 }
 
 export interface ISection {
-  name: string;
-  description: string;
-  file: IFileType;
-  id: string;
+	name: string;
+	description: string;
+	file: IFileType;
+	id: string;
 }
 
 export interface IFaq {
-  name: string;
-  description: string;
-  id: string;
+	name: string;
+	description: string;
+	id: string;
 }
 
 export interface IComponent {
-  name: string;
-  fileEntity: IFileType;
-  id: string;
+	name: string;
+	fileEntity: IFileType;
+	id: string;
 }
 
 export interface IAccessory {
-  name: string;
-  fileResponses: IFileType[];
-  id: string;
+	name: string;
+	fileResponses: IFileType[];
+	id: string;
 }
 
 export interface ICharacteristics {
-  name: string;
-  characteristicDetails: ICharacteristicsDetail[];
-  id: string;
+	name: string;
+	characteristicDetails: ICharacteristicsDetail[];
+	id: string;
 }
 
 export interface ICharacteristicsDetail {
-  name: string;
-  detail: string;
-  id: string;
+	name: string;
+	detail: string;
+	id: string;
 }
